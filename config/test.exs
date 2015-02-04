2ba0ed6ff708aa6ae4790498c389d88c47e5d78f
Paul Smith <catalystmediastudios@gmail.com>
2015-02-04 15:27:49 -0500

    Add Repo to test environment

--- config/test.exs
+++ config/test.exs
@@ -3,5 +3,9 @@ use Mix.Config
 config :constable_api, ConstableApi.Endpoint,
   http: [port: System.get_env("PORT") || 4001]
 
+config :constable_api, ConstableApi.Repo,
+  database: "constable_api_test",
+  hostname: "localhost"
+
 # Print only warnings and errors during test
 config :logger, level: :warn
