9d1e290ef958b3e93c624a82c7a6294bbdd44575
Paul Smith <catalystmediastudios@gmail.com>
2015-07-24 15:15:46 -0400

    Add Repo and model aliases to .iex.exs
    This file is loaded automatically when calling `iex -S mix` (the console)
    This makes using the Repo a lot easier to use from the console.

---
+++ .iex.exs
@@ -0,0 +1,10 @@
+alias Constable.Repo
+alias Constable.Announcement
+alias Constable.AnnouncementInterest
+alias Constable.Comment
+alias Constable.Interest
+alias Constable.Subscription
+alias Constable.User
+alias Constable.UserInterest
+
+import Ecto.Query
