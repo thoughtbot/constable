1f956df521afa3a38ab391730ec4d45417d5f20f
Paul Smith <catalystmediastudios@gmail.com>
2015-08-13 15:07:06 -0600

    Add Repo.get_or_insert/1
    This makes it easy to either get a record or insert it

--- lib/repo.ex
+++ lib/repo.ex
@@ -8,4 +8,12 @@ defmodule Constable.Repo do
     __MODULE__.all(from record in query, select: count(record.id))
     |> List.first
   end
+
+  def get_or_insert(%{model: %{__struct__: model}, changes: changes} = changeset) do
+    record = __MODULE__.get_by(model, changes)
+    case record do
+      nil -> __MODULE__.insert!(changeset)
+      _ -> record
+    end
+  end
 end
