c7d6c6d8b2e8f4204f48c987b223f0585b8442de
Blake Williams <blake@blakewilliams.me>
2015-09-21 13:51:11 -0400

    Remove old views

--- web/views/comment_view.ex
+++
@@ -1,9 +0,0 @@
-defmodule Constable.CommentView do
-  use Constable.Web, :view
-
-  @attributes ~W(id body user announcement_id inserted_at)a
-
-  def render("show.json", %{comment: comment}) do
-    comment |> Map.take(@attributes)
-  end
-end
