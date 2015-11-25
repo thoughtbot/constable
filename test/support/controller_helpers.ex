cda1141ef19286d9f30bf261aca18b47a76cf7fa
Paul Smith <catalystmediastudios@gmail.com>
2015-11-25 12:18:13 -0500

    Remove unused controller test helpers

--- test/support/controller_helpers.ex
+++
@@ -1,9 +0,0 @@
-defmodule Constable.ControllerHelpers do
-  import ExUnit.Assertions
-  import Plug.Conn
-
-  def assert_redirected(conn, location) do
-    assert conn.status in [301, 302]
-    assert get_resp_header(conn, "Location") == [location]
-  end
-end
