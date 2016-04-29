00839276492bbbb76511266c85efd90076fe884b
Joshua Clayton <joshua.clayton@gmail.com>
2016-04-29 15:36:26 -0400

    Remove Constable.Queries.Subscription.for_user
    2edcb2b0014a804d1d01161d3c20b3efc39d0683 removed all code using this
    function.

--- web/models/queries/subscription.ex
+++ web/models/queries/subscription.ex
@@ -2,10 +2,6 @@ defmodule Constable.Queries.Subscription do
   alias Constable.Subscription
   import Ecto.Query
 
-  def for_user(user_id) do
-    from s in Subscription, where: s.user_id == ^user_id
-  end
-
   def for_announcement(announcement_id) do
     from s in Subscription,
       where: s.announcement_id == ^announcement_id,
