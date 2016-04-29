7dddbfd69d7b612a79c485ffd7ee85653ce3507c
Paul Smith <paulcsmith0218@gmail.com>
2016-04-29 13:44:06 -0400

    Clear cache when refreshing with Turbolinks
    This will remove a slight flicker when subscribing/unsubscribing to many
    interests.

--- web/static/js/turbolinks-refresh.js
+++ web/static/js/turbolinks-refresh.js
@@ -26,6 +26,7 @@ const sendLinkWithAjax = function(link) {
 
 const refreshPageWithTurbolinks = function() {
   var path = window.location.pathname + window.location.search;
+  Turbolinks.clearCache();
   Turbolinks.visit(path);
 }
 
