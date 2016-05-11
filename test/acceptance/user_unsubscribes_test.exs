34706e41086d00118ff202d19ae4cbcdc54cdee8
Melanie Gilman <melanie@thoughtbot.com>
2016-05-11 10:01:06 -0500

    Display flash messages in login layout
    Resolves issue that when unsubscribing while logged out, it was not obvious that
    the unsubscribe was successful because the confirmation flash message was not
    displayed on the page.

---
+++ test/acceptance/user_unsubscribes_test.exs
@@ -0,0 +1,38 @@
+defmodule Constable.UserUnsubscribesTest do
+  use Constable.AcceptanceCase
+
+  test "shows unsubscribe message when logged in", %{session: session} do
+    announcement = insert(:announcement)
+    subscription = insert(:subscription, announcement: announcement)
+    user = insert(:user)
+
+    session
+    |> visit(unsubscribe_path(Endpoint, :show, subscription.token, as: user))
+
+    assert has_unsubscribed_flash_message?(session)
+    assert has_announcement_title?(session, announcement.title)
+  end
+
+  test "shows unsubscribed message when logged out", %{session: session} do
+    subscription = insert(:subscription)
+    session
+    |> visit(unsubscribe_path(Endpoint, :show, subscription.token))
+
+    assert has_unsubscribed_flash_message?(session)
+    assert has_login_button?(session)
+  end
+
+  defp has_unsubscribed_flash_message?(session) do
+    session
+    |> find(".flash")
+    |> has_text?("You've been unsubscribed")
+  end
+
+  defp has_announcement_title?(session, text) do
+    session |> find("h1[data-role=title]") |> has_text?(text)
+  end
+
+  defp has_login_button?(session) do
+    session |> find("a.sign-in-link") |> has_text?("Sign in")
+  end
+end
