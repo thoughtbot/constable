76f2e231729983631f0655622329aa0b5c756b3f
Ryan Coughlin <ryan@ryancoughlin.com>
2016-07-08 11:32:36 -0400

    Change "about now" to "just now" on comment timestamp

--- web/views/shared_view.ex
+++ web/views/shared_view.ex
@@ -14,7 +14,7 @@ defmodule Constable.SharedView do
   end
 
   defp rel_from_now({0, {0, 0, sec}}) when sec < 30,
-    do: "about now"
+    do: "just now"
   defp rel_from_now({0, {0, min, _}}) when min < 2,
     do: "1 minute ago"
   defp rel_from_now({0, {0, min, _}}),
@@ -26,7 +26,7 @@ defmodule Constable.SharedView do
   defp rel_from_now({1, {_, _, _}}),
     do: "1 day ago"
   defp rel_from_now({day, {_, _, _}}) when day < 0,
-    do: "about now"
+    do: "just now"
   defp rel_from_now({day, {_, _, _}}),
     do: "#{day} days ago"
 
