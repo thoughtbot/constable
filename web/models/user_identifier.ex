82e4c5e015cf7d2eddab9c6c0051ca7872256f8a
German Velasco <germsvel@gmail.com>
2017-12-22 09:52:38 -0500

    Address Phoenix.Token warning
    Phoenix.Token throws a warning (since 1.3) to set a `max_age` when
    verifying a token. We set the max age to one week.

--- web/models/user_identifier.ex
+++ web/models/user_identifier.ex
@@ -1,9 +1,12 @@
 defmodule Constable.UserIdentifier do
+  @one_week 86400 * 7
+  @salt "user_id"
+
   def sign_user_id(conn, user_id) do
-    Phoenix.Token.sign(conn, "user_id", user_id)
+    Phoenix.Token.sign(conn, @salt, user_id)
   end
 
   def verify_signed_user_id(conn) do
-    Phoenix.Token.verify(conn, "user_id", conn.cookies["user_id"])
+    Phoenix.Token.verify(conn, @salt, conn.cookies["user_id"], max_age: @one_week)
   end
 end
