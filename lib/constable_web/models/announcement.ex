24914e33c11b08cb3a8cf2481abd700b67d6f2ed
German Velasco <germsvel@users.noreply.github.com>
2018-07-20 10:20:39 -0400

    Return last discussed announcements first (#499)
    A previous commit c7cec8962f6b9c7f08b8b54f26991eed26f79fcb introduced an
    ordering of announcements for `Announcement.search/3` in order to fix an
    intermittently failing test. But that also introduced a bug. The order
    of announcements returned from the search were no longer ordered with the
    `Announcement.last_discussed_first/1`. We fix that here by removing the
    ordering on the `search/3` function and relying on
    `last_discussed_first/1` for the search controllers, both for the web
    and the api.

--- lib/constable_web/models/announcement.ex
+++ lib/constable_web/models/announcement.ex
@@ -60,7 +60,6 @@ defmodule Constable.Announcement do
       full_join: i in assoc(a, :interests),
       group_by: a.id,
       having: fragment("NOT ARRAY_AGG(?) @> ?", i.name, ^excludes),
-      order_by: a.id,
       where: fragment("to_tsvector('english', ?) || to_tsvector('english', ?) @@ plainto_tsquery('english', ?)",
         a.title,
         a.body,
