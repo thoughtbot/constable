f5a6d6c3c8832cbdab669e4eb42085479e48e428
Matt Jankowski <mjankowski@thoughtbot.com>
2018-09-07 13:31:37 -0400

    Update deprecated method name: `Earmark.as_html`

--- lib/constable/markdown.ex
+++ lib/constable/markdown.ex
@@ -1,7 +1,9 @@
 defmodule Constable.Markdown do
+  alias Earmark.Options
+
   def to_html(markdown) do
     markdown
-    |> Earmark.to_html
+    |> Earmark.as_html!(%Options{smartypants: false})
     |> HtmlSanitizeEx.basic_html
   end
 end
--- test/lib/constable/markdown_test.exs
+++ test/lib/constable/markdown_test.exs
@@ -4,12 +4,23 @@ defmodule Constable.MarkdownTest do
   describe "to_html/1" do
     test "filters out JavaScript" do
       html = """
+      <code>
       <script>alert("Gonna steal your data!");</script>
       <a href="javascript:alert("Stolen!")>Click please!</a>
+      </code>
       """
       |> Constable.Markdown.to_html
 
-      assert html == ~s[alert("Gonna steal your data!");<a>Click please!</a>]
+      assert html == ~s[<code>\nalert("Gonna steal your data!");\n<a>Click please!</a>\n</code>]
+    end
+
+    test "converts markdown" do
+      html = """
+      This is a paragraph
+      """
+      |> Constable.Markdown.to_html
+
+      assert html == "<p>This is a paragraph</p>\n"
     end
   end
 end
