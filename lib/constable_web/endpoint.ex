33383a2a736b9c128752fddb673d273acdc070e9
Matt Jankowski <mjankowski@thoughtbot.com>
2018-09-12 10:58:06 -0400

    Update constable_web/endpoint to match project style

--- lib/constable_web/endpoint.ex
+++ lib/constable_web/endpoint.ex
@@ -7,20 +7,23 @@ defmodule ConstableWeb.Endpoint do
     plug Phoenix.Ecto.SQL.Sandbox
   end
 
+  # Serve at "/" the static files from "priv/static" directory.
+  #
+  # You should set gzip to true if you are running phoenix.digest
+  # when deploying your static files in production.
   plug Plug.Static,
-    at: "/", from: :constable,
-    gzip: true,
-    only: ~w(css fonts images js favicon.ico)
+    at: "/", from: :constable, gzip: true,
+    only: ~w(css fonts images js favicon.ico robots.txt)
 
-  plug Plug.Logger
-
-  # Code reloading will only work if the :code_reloader key of
-  # the :phoenix application is set to true in your config file.
+  # Code reloading can be explicitly enabled under the
+  # :code_reloader configuration of your endpoint.
   if code_reloading? do
     socket "/phoenix/live_reload/socket", Phoenix.LiveReloader.Socket
     plug Phoenix.LiveReloader
     plug Phoenix.CodeReloader
   end
+
+  plug Plug.Logger
 
   plug Plug.Parsers,
     parsers: [:urlencoded, :multipart, :json],
@@ -30,10 +33,27 @@ defmodule ConstableWeb.Endpoint do
   plug Plug.MethodOverride
   plug Plug.Head
 
+  # The session will be stored in the cookie and signed,
+  # this means its contents can be read but not tampered with.
+  # Set :encryption_salt if you would also like to encrypt it.
   plug Plug.Session,
     store: :cookie,
     key: "_constable_key",
     signing_salt: "/CEisxlR"
 
   plug ConstableWeb.Router
+
+  @doc """
+  Callback invoked for dynamically configuring the endpoint.
+  It receives the endpoint configuration and checks if
+  configuration should be loaded from the system environment.
+  """
+  def init(_key, config) do
+    if config[:load_from_system_env] do
+      port = System.get_env("PORT") || raise "expected the PORT environment variable to be set"
+      {:ok, Keyword.put(config, :http, [:inet6, port: port])}
+    else
+      {:ok, config}
+    end
+  end
 end
