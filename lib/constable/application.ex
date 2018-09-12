6f43fecc8ee7d33c7f68f32a3ab8d64935135a83
Matt Jankowski <mjankowski@thoughtbot.com>
2018-09-12 09:42:58 -0400

    Update Constable.Application to match new phoenix style

--- lib/constable/application.ex
+++ lib/constable/application.ex
@@ -1,10 +1,10 @@
 defmodule Constable.Application do
   use Application
 
-  # See http://elixir-lang.org/docs/stable/elixir/Application.html
+  # See https://hexdocs.pm/elixir/Application.html
   # for more information on OTP Applications
   def start(_type, _args) do
-    import Supervisor.Spec, warn: false
+    import Supervisor.Spec
 
     unless Mix.env == "production" do
       Envy.auto_load
@@ -12,16 +12,17 @@ defmodule Constable.Application do
 
     setup_dependencies()
 
+    # Define workers and child supervisors to be supervised
     children = [
+      # Start the Ecto repository
+      supervisor(Constable.Repo, []),
       # Start the endpoint when the application starts
-      worker(ConstableWeb.Endpoint, []),
-      worker(Constable.Repo, [])
-
-      # Here you could define other workers and supervisors as children
+      supervisor(ConstableWeb.Endpoint, []),
+      # Start your own worker by calling: Constable.Worker.start_link(arg1, arg2, arg3)
       # worker(Constable.Worker, [arg1, arg2, arg3]),
     ]
 
-    # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
+    # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
     opts = [strategy: :one_for_one, name: Constable.Supervisor]
     Supervisor.start_link(children, opts)
