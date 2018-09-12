76678e285950800f4bf1af047288798d0a4196f3
Matt Jankowski <mjankowski@thoughtbot.com>
2018-09-12 09:41:29 -0400

    Update config/dev to match new phoenix style

--- config/dev.exs
+++ config/dev.exs
@@ -6,16 +6,28 @@ config :constable, ConstableWeb.Endpoint,
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
-  watchers: [node: ["node_modules/brunch/bin/brunch", "watch", "--stdin", cd: Path.expand("../assets", __DIR__)]],
+  watchers: [node: ["node_modules/brunch/bin/brunch", "watch", "--stdin",
+                    cd: Path.expand("../assets", __DIR__)]]
+
+# Watch static and templates for browser reloading.
+config :constable, ConstableWeb.Endpoint,
   live_reload: [
     patterns: [
-      ~r{priv/static/.*(js|css|png|jpeg|jpg|gif)$},
+      ~r{priv/static/.*(js|css|png|jpeg|jpg|gif|svg)$},
       ~r{priv/gettext/.*(po)$},
       ~r{lib/constable_web/views/.*(ex)$},
       ~r{lib/constable_web/templates/.*(eex)$}
     ]
   ]
 
+# Do not include metadata nor timestamps in development logs
+config :logger, :console, format: "[$level] $message\n"
+
+# Set a higher stacktrace during development. Avoid configuring such
+# in production as building large stacktraces may be expensive.
+config :phoenix, :stacktrace_depth, 20
+
+# Configure your database
 config :constable, Constable.Repo,
   adapter: Ecto.Adapters.Postgres,
   database: "constable_api_development",
@@ -27,10 +39,3 @@ config :constable, Constable.Mailer,
 config :constable, :shubox_script_url, "http://shubox.io/x/a7c92ded.js"
 
 config :honeybadger, :environment_name, :dev
-
-# Do not include metadata nor timestamps in development logs
-config :logger, :console, format: "[$level] $message\n"
-
-# Set a higher stacktrace during development. Avoid configuring such
-# in production as building large stacktraces may be expensive.
-config :phoenix, :stacktrace_depth, 20
