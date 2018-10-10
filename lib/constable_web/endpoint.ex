78241bc0c99ed436031e606f5f0770208444f649
Matt Jankowski <mjankowski@thoughtbot.com>
2018-10-10 10:09:46 -0400

    Update socket to phoenix 1.4 style

--- lib/constable_web/endpoint.ex
+++ lib/constable_web/endpoint.ex
@@ -1,47 +1,55 @@
 defmodule ConstableWeb.Endpoint do
   use Phoenix.Endpoint, otp_app: :constable
 
-  socket "/socket", ConstableWeb.UserSocket
+  socket("/socket", ConstableWeb.UserSocket,
+    websocket: [check_origin: false],
+    longpoll: true
+  )
 
   if Application.get_env(:constable, :sql_sandbox) do
-    plug Phoenix.Ecto.SQL.Sandbox
+    plug(Phoenix.Ecto.SQL.Sandbox)
   end
 
   # Serve at "/" the static files from "priv/static" directory.
   #
   # You should set gzip to true if you are running phoenix.digest
   # when deploying your static files in production.
-  plug Plug.Static,
-    at: "/", from: :constable, gzip: true,
+  plug(Plug.Static,
+    at: "/",
+    from: :constable,
+    gzip: true,
     only: ~w(css fonts images js favicon.ico robots.txt)
+  )
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
   if code_reloading? do
-    socket "/phoenix/live_reload/socket", Phoenix.LiveReloader.Socket
-    plug Phoenix.LiveReloader
-    plug Phoenix.CodeReloader
+    socket("/phoenix/live_reload/socket", Phoenix.LiveReloader.Socket)
+    plug(Phoenix.LiveReloader)
+    plug(Phoenix.CodeReloader)
   end
 
-  plug Plug.Logger
+  plug(Plug.Logger)
 
-  plug Plug.Parsers,
+  plug(Plug.Parsers,
     parsers: [:urlencoded, :multipart, :json],
     pass: ["*/*"],
     json_decoder: Poison
+  )
 
-  plug Plug.MethodOverride
-  plug Plug.Head
+  plug(Plug.MethodOverride)
+  plug(Plug.Head)
 
   # The session will be stored in the cookie and signed,
   # this means its contents can be read but not tampered with.
   # Set :encryption_salt if you would also like to encrypt it.
-  plug Plug.Session,
+  plug(Plug.Session,
     store: :cookie,
     key: "_constable_key",
     signing_salt: "/CEisxlR"
+  )
 
-  plug ConstableWeb.Router
+  plug(ConstableWeb.Router)
 
   @doc """
   Callback invoked for dynamically configuring the endpoint.
