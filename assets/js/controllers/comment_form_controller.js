733a6c19acd03fb2a7b0971a46ea178f8494ed3d
Matt Jankowski <mjankowski@thoughtbot.com>
2018-10-25 21:56:08 -0400

    Skip comment-form controller on comment edit page

--- assets/js/controllers/comment_form_controller.js
+++ assets/js/controllers/comment_form_controller.js
@@ -5,19 +5,25 @@ const disableForm = (form) => form.children(':input').attr('disabled', 'disabled
 const enableForm = (form) => form.children(':input').removeAttr('disabled')
 
 export default class extends Controller {
-  submit(event) {
-    event.preventDefault()
-    const form = $(this.element)
+  initialize() {
+    this._initialized = true
+  }
 
-    $.ajax({
-      type: 'POST',
-      url: form.attr('action'),
-      data: form.serialize(),
-      beforeSend: () => disableForm(form),
-    })
-    .done(() => {
-      resetForm(form[0])
-      enableForm(form)
-    })
+  submit(event) {
+    if (this._initialized) {
+      event.preventDefault()
+      const form = $(this.element.querySelector('form'))
+
+      $.ajax({
+        type: 'POST',
+        url: form.attr('action'),
+        data: form.serialize(),
+        beforeSend: () => disableForm(form),
+      })
+      .done(() => {
+        resetForm(form[0])
+        enableForm(form)
+      })
+    }
   }
 }
