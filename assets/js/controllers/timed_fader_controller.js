77a1b0e653d69b5818fca43dbde18825475150ff
Matt Jankowski <mjankowski@thoughtbot.com>
2018-10-25 22:31:46 -0400

    Add timed fader controller for recipient names hover

---
+++ assets/js/controllers/timed_fader_controller.js
@@ -0,0 +1,19 @@
+import { Controller } from "stimulus"
+
+export default class extends Controller {
+  static targets = ["userNames"]
+
+  fadeIn() {
+    const element = $(this.userNamesTarget)
+
+    this._timer = setTimeout(function() {
+      element.fadeIn(150)
+    }, 150)
+  }
+
+  fadeOut() {
+    const element = $(this.userNamesTarget)
+    element.fadeOut(150)
+    clearTimeout(this._timer)
+  }
+}
