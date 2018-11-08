cb4cb4f42d670f14e2813d732a95cbb91c27f68a
Matt Jankowski <mjankowski@thoughtbot.com>
2018-11-08 10:50:55 -0500

    Update lib/ to match newly generated phoenix app style

--- lib/constable/repo.ex
+++ lib/constable/repo.ex
@@ -2,22 +2,11 @@ defmodule Constable.Repo do
   use Ecto.Repo,
     otp_app: :constable,
     adapter: Ecto.Adapters.Postgres
+
   use Scrivener, page_size: 30
   import Ecto.Query
 
-  @doc """
-  Dynamically loads the repository url from the
-  DATABASE_URL environment variable.
-  """
-  def init(_, opts) do
-    {:ok, Keyword.put(opts, :url, System.get_env("DATABASE_URL"))}
-  end
-
   def count(query) do
     one!(from record in query, select: count(record.id))
-  end
-
-  def count_distinct(query) do
-    one!(from record in query, select: count(record.id, :distinct))
   end
 end
--- lib/constable_web.ex
+++ lib/constable_web.ex
@@ -39,7 +39,7 @@ defmodule ConstableWeb do
 
       # Import convenience functions from controllers
       import Phoenix.Controller,
-        only: [get_csrf_token: 0, get_flash: 2, view_module: 1]
+        only: [get_flash: 1, get_flash: 2, view_module: 1]
 
       import ConstableWeb.ErrorHelpers
       import ConstableWeb.Gettext
