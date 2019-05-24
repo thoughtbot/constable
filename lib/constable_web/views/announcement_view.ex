2691a2f9350580ed4c1a6bb1f8d785155e0534e7
German Velasco <germsvel@gmail.com>
2019-05-24 16:31:25 -0400

    Make user names clickable to see their announcements
    This leverages the `/announcements#index` route with the query params
    `user_id` that we introduced in order to display a user's own
    announcements. We use it to display the list of announcements for a
    any user when we click on their name.
    
    The only strange thing about this is that we leave the nav bar in a
    completely unselected state, since seeing another user's announcements
    does not have an equivalent "tab" as we might have for "All
    announcements" or "Your announcements".

--- lib/constable_web/views/announcement_view.ex
+++ lib/constable_web/views/announcement_view.ex
@@ -16,11 +16,21 @@ defmodule ConstableWeb.AnnouncementView do
   def comma_separated_interest_names(_), do: ""
 
   def class_for("all", %{params: %{"all" => "true"}}), do: "selected"
-  def class_for("your announcements", %{params: %{"user_id" => _}}), do: "selected"
+
+  def class_for("your announcements", conn = %{params: %{"user_id" => id}}) do
+    if current_user_same_as_announcements_user?(conn, id) do
+      "selected"
+    end
+  end
+
   def class_for("your interests", %{params: %{"all" => "true"}}), do: nil
   def class_for("your interests", %{params: %{"user_id" => _}}), do: nil
   def class_for("your interests", _), do: "selected"
   def class_for(_, _), do: nil
+
+  defp current_user_same_as_announcements_user?(conn, announcements_user_id) do
+    "#{conn.assigns[:current_user].id}" == announcements_user_id
+  end
 
   def interest_count_for(user) do
     length(user.interests)
