bb9cd6878f6f969600dfd146560af262a292751e
Wil Hall <wil@wilhall.com>
2019-09-16 21:39:22 -0400

    Remove hub profile link

--- lib/constable_web/views/settings_view.ex
+++ lib/constable_web/views/settings_view.ex
@@ -1,8 +1,3 @@
 defmodule ConstableWeb.SettingsView do
   use ConstableWeb, :view
-  alias Constable.Services.HubProfile
-
-  def hub_profile_url(user) do
-    HubProfile.profile_url(user)
-  end
 end
