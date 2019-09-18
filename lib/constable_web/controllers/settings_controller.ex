edc5e87f1d77f6a2a220cce1b6eb586138a2c078
Tyson Gach <tyson@tysongach.com>
2019-09-18 11:38:17 -0400

    Fix Settings page, remove Settings modal
    This fixes the rendering of the Settings page, which was not rendered
    using a layout and therefore lacked CSS styling.
    
    In conjunction, we remove the modal UI from the viewing of Settings and
    instead leverage a standard page view. Modals often pose accessibility
    issues (ours did) and it meant that did not have a URL to point at for
    settings (our emails point to a `/settings` URL, though).
    
    Fixes: https://github.com/thoughtbot/constable/issues/700

--- lib/constable_web/controllers/settings_controller.ex
+++ lib/constable_web/controllers/settings_controller.ex
@@ -5,7 +5,7 @@ defmodule ConstableWeb.SettingsController do
 
   def show(conn, _params) do
     changeset = User.settings_changeset(conn.assigns.current_user)
-    render conn, "show.html", changeset: changeset, layout: false
+    render conn, "show.html", changeset: changeset
   end
 
   def update(conn, %{"user" => user_params}) do
@@ -15,12 +15,12 @@ defmodule ConstableWeb.SettingsController do
       {:ok, _user} ->
         conn
         |> put_flash(:success, "YES!")
-        |> render("show.html", changeset: changeset, layout: false)
+        |> render("show.html", changeset: changeset)
       {:error, changeset} ->
         conn
         |> put_status(:unprocessable_entity)
         |> put_flash(:error, gettext("Something went wrong!"))
-        |> render("show.html", changeset: changeset, layout: false)
+        |> render("show.html", changeset: changeset)
     end
   end
 end
--- test/acceptance/user_controls_settings_test.exs
+++ test/acceptance/user_controls_settings_test.exs
@@ -1,66 +1,11 @@
 defmodule ConstableWeb.UserControlsSettingsTest do
   use ConstableWeb.AcceptanceCase
 
-  @modal_window css("[data-role=settings-modal]")
-  @settings_link css("[data-role=settings-link]")
-  @closing_icon css("[data-role=closing-icon]")
-
   test "user views their settings", %{session: session} do
     user = insert(:user)
 
     session
-    |> visit(Routes.announcement_path(Endpoint, :new, as: user.id))
-    |> open_settings_modal
-
-    assert showing_modal?(session)
-  end
-
-  test "user closes the settings modal with icon", %{session: session} do
-    user = insert(:user)
-
-    session
-    |> visit(Routes.announcement_path(Endpoint, :new, as: user.id))
-    |> open_settings_modal
-    |> assert_has(@modal_window)
-    |> close_modal_with_icon
-
-    refute showing_modal?(session)
-  end
-
-  test "user closes the settings modal with esc", %{session: session} do
-    user = insert(:user)
-
-    session
-    |> visit(Routes.announcement_path(Endpoint, :new, as: user.id))
-    |> open_settings_modal
-    |> assert_has(@modal_window)
-    |> close_modal_with_esc
-
-    refute showing_modal?(session)
-  end
-
-  defp open_settings_modal(session) do
-    session
-    |> click(@settings_link)
-
-    session
-  end
-
-  defp close_modal_with_icon(session) do
-    session
-    |> click(@closing_icon)
-
-    session
-  end
-
-  defp close_modal_with_esc(session) do
-    session
-    |> send_keys([:escape])
-
-    session
-  end
-
-  defp showing_modal?(session) do
-    session |> has?(@modal_window)
+    |> visit(Routes.settings_path(Endpoint, :show, as: user.id))
+    |> has_text?("Profile and Settings")
   end
 end
