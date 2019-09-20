81a815d3c582b04de01a567bf223b9a06fddaec6
Wil Hall <wil@wilhall.com>
2019-09-20 15:54:41 -0400

    Add interest count to interests page

--- lib/constable_web/views/interest_view.ex
+++ lib/constable_web/views/interest_view.ex
@@ -2,4 +2,8 @@ defmodule ConstableWeb.InterestView do
   use ConstableWeb, :view
 
   import Constable.User, only: [interested_in?: 2]
+
+  def interest_count_for(user) do
+    length(user.interests)
+  end
 end
