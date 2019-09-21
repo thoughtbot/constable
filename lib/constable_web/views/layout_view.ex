c54a0b60aeb8cc1a848b82e5b1f370c7084dd7a1
Wil Hall <wil@wilhall.com>
2019-09-21 10:03:03 -0400

    Allow rendering of custom content containers

--- lib/constable_web/views/layout_view.ex
+++ lib/constable_web/views/layout_view.ex
@@ -1,3 +1,18 @@
 defmodule ConstableWeb.LayoutView do
   use ConstableWeb, :view
+
+  def content_container(conn, assigns \\ [], do: contents) do
+    container_assigns =
+      assigns
+      |> Map.to_list()
+      |> Keyword.put(:conn, conn)
+      |> Keyword.put(:contents, contents)
+
+    render_existing(
+      assigns.view_module,
+      "_content_container.html",
+      container_assigns
+    ) ||
+      render("_content_container.html", container_assigns)
+  end
 end
