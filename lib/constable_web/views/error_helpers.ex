5214088f4847e332119a0ece129e138ce229adaa
Wil Hall <wil@wilhall.com>
2019-10-11 09:49:38 -0400

    Set role to alert on form error

--- lib/constable_web/views/error_helpers.ex
+++ lib/constable_web/views/error_helpers.ex
@@ -10,7 +10,7 @@ defmodule ConstableWeb.ErrorHelpers do
   """
   def error_tag(form, field) do
     Enum.map(Keyword.get_values(form.errors, field), fn error ->
-      content_tag(:span, translate_error(error), id: "#{field}_error", class: "help-block")
+      content_tag(:span, translate_error(error), id: "#{field}_error", class: "help-block", role: "alert")
     end)
   end
 
