e00a97bc53775f333ed8be91f916f6ae1de7fc6f
Drew Volz <volz.drew@gmail.com>
2020-02-24 12:55:31 -0800

    Subscribe users to announcements they commented on (#628)
    What changed?
    =============
    
    We automatically subscribe users to announcements when they comment on
    them.
    
    We add another step in the `CommentCreator.create` function to subscribe
    the user to the article they are commenting on. We then update the UI
    when sending the html for the new comment via the `LiveHtmlChannel`.
    
    Co-authored-by: German Velasco <germsvel@users.noreply.github.com>

--- lib/constable_web/channels/live_html_channel.ex
+++ lib/constable_web/channels/live_html_channel.ex
@@ -8,14 +8,27 @@ defmodule ConstableWeb.LiveHtmlChannel do
   end
 
   def handle_out("new-comment", %{comment: comment}, socket) do
+    current_user = socket.assigns[:current_user]
+    announcement_id = comment.announcement_id
+
     payload = %{
-      announcement_id: comment.announcement_id,
-      comment_html: render_comment_html(comment, socket.assigns[:current_user])
+      announcement_id: announcement_id,
+      comment_html: render_comment_html(comment, current_user),
+      subscribed: comment.user_id == current_user.id,
+      unsubscribe_button_html: render_unsubscribe_button_html(announcement_id)
     }
 
     push(socket, "new-comment", payload)
 
     {:noreply, socket}
+  end
+
+  defp render_unsubscribe_button_html(announcement_id) do
+    Phoenix.View.render_to_string(
+      ConstableWeb.AnnouncementView,
+      "_unsubscribe_button.html",
+      announcement_id: announcement_id
+    )
   end
 
   defp render_comment_html(comment, current_user) do
