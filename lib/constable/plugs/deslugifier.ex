74cf78fcc27329c5efc39483ea7b046eed724ffc
German Velasco <germsvel@users.noreply.github.com>
2021-04-09 14:02:41 -0400

    Extract Slug.deslugify module (#984)
    In preparation for other work, we separate the `deslugify` functionality
    from `Plug.Deslugifier` into its own `Slug` module.

--- lib/constable/plugs/deslugifier.ex
+++ lib/constable/plugs/deslugifier.ex
@@ -1,4 +1,6 @@
 defmodule Constable.Plugs.Deslugifier do
+  alias Constable.Slug
+
   def init(opts) do
     case Keyword.get(opts, :slugified_key) do
       nil -> raise "Must provide a :slugified_key to #{inspect(__MODULE__)}"
@@ -8,17 +10,10 @@ defmodule Constable.Plugs.Deslugifier do
 
   def call(conn, key) do
     with {:ok, slugified_id} <- Map.fetch(conn.params, key),
-         {:ok, id} <- deslugify(slugified_id) do
+         {:ok, id} <- Slug.deslugify(slugified_id) do
       put_in(conn, [Access.key!(:params), key], id)
     else
       _ -> conn
-    end
-  end
-
-  defp deslugify(slugified_id) do
-    case Integer.parse(slugified_id) do
-      {int, _} when int > 0 -> {:ok, int}
-      _ -> :error
     end
   end
 end
